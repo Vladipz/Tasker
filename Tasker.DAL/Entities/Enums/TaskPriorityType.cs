069438ef6dfa73c8282ec40307476cbcb090d526
Vladyslav Danilchuk <vladdanilchuk9@gmail.com>
2024-09-03 11:23:17 +0300

    add enums to DAL

---
+++ Tasker.DAL/Entities/Enums/TaskPriorityType.cs
@@ -0,0 +1,25 @@
+namespace Tasker.DAL.Entities.Enums
+{
+    public enum TaskPriorityType
+    {
+        /// <summary>
+        /// No priority is assigned.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Low priority level.
+        /// </summary>
+        Low = 1,
+
+        /// <summary>
+        /// Medium priority level.
+        /// </summary>
+        Medium = 2,
+
+        /// <summary>
+        /// High priority level.
+        /// </summary>
+        High = 3,
+    }
+}
