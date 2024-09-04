b7046861e2e87361867052bacba78a986e6de29d
Vladyslav Danilchuk <vladdanilchuk9@gmail.com>
2024-09-04 20:41:21 +0300

    add fake getUserId method

---
+++ Tasker.API/Helpers/Extensions.cs
@@ -0,0 +1,11 @@
+namespace Tasker.API.Helpers
+{
+    public static class Extensions
+    {
+        public static Guid GetUserId(this HttpContext httpContext)
+        {
+            // TODO: Implement this method
+            return Guid.Parse("aec78d50-3e53-4220-8207-7286e6b5db7e");
+        }
+    }
+}
