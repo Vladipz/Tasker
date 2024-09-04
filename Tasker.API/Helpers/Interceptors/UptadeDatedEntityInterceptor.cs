6ca555a10aef4dac9fb00d0969af5854833ee8c0
Vladyslav Danilchuk <vladdanilchuk9@gmail.com>
2024-09-05 01:40:17 +0300

    done intercepror

--- Tasker.API/Helpers/Interceptors/UptadeDatedEntityInterceptor.cs
+++ Tasker.API/Helpers/Interceptors/UptadeDatedEntityInterceptor.cs
@@ -24,13 +24,16 @@ namespace Tasker.API.Helpers.Interceptors
         private static void UpdateAuditableEntities(DbContext context)
         {
             var utcNow = DateTimeOffset.Now;
-            var entities = context.ChangeTracker.Entries<IDatedEntity>().ToList();
 
-            foreach (EntityEntry<IDatedEntity> entry in entities)
+            // HACK: I use interface from data access layer in the API project
+            var entities = context.ChangeTracker.Entries<DAL.Interfaces.IDatedEntity>().ToList();
+
+            foreach (EntityEntry<DAL.Interfaces.IDatedEntity> entry in entities)
             {
                 if (entry.State == EntityState.Added)
                 {
                     SetCurrentPropertyValue(entry, nameof(IDatedEntity.CreatedAt), utcNow);
+                    SetCurrentPropertyValue(entry, nameof(IDatedEntity.UpdatedAt), utcNow);
                 }
 
                 if (entry.State == EntityState.Modified)
