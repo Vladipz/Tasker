ac77561be18f1e00dd3b803b5cd30e1c26b2c423
Vladyslav Danilchuk <vladdanilchuk9@gmail.com>
2024-09-06 14:47:22 +0300

    Add authentication

--- Tasker.API/Endpoints/AuthenticationEndpoint.cs
+++ Tasker.API/Endpoints/AuthenticationEndpoint.cs
@@ -1,4 +1,13 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+using ErrorOr;
+
+using Microsoft.IdentityModel.Tokens;
+
 using Tasker.API.Contracts.Requests;
+using Tasker.API.Contracts.Responses;
 using Tasker.API.Mapping;
 using Tasker.BLL.Interfaces;
 
@@ -15,6 +24,20 @@ namespace Tasker.API.Endpoints
                 var result = await userService.RegisterAsync(request.ToCreateModel());
                 return result.Match(
                     user => Results.Created(),
+                    error => error.ToResponse());
+            });
+
+            group.MapPost("/login", async (
+                LoginRequest request,
+                IUserService userService,
+                ITokenService tokenService) =>
+            {
+                var result = await userService.ReadAsync(request.Username)
+                    .ThenAsync(user => userService.CheckPasswordAsync(user, request.Password))
+                    .Then(user => tokenService.GenerateToken(user));
+
+                return result.Match(
+                    token => Results.Ok(token.ToResponse()),
                     error => error.ToResponse());
             });
         }
