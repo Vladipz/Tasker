e2737d03755d940e0646f59bedf4fa34a1f0649c
Vladyslav Danilchuk <vladdanilchuk9@gmail.com>
2024-09-06 14:36:58 +0300

    add user carvice interface

--- Tasker.BLL/Interfaces/IUserService.cs
+++ Tasker.BLL/Interfaces/IUserService.cs
@@ -7,6 +7,9 @@ namespace Tasker.BLL.Interfaces
     public interface IUserService
     {
         Task<ErrorOr<Created>> RegisterAsync(UserCreateModel model);
-        // Task<ErrorOr<>> LoginAsync(UserLoginModel model);
+
+        Task<ErrorOr<TaskerUserModel>> ReadAsync(string username);
+
+        Task<ErrorOr<TaskerUserModel>> CheckPasswordAsync(TaskerUserModel userModel, string password);
     }
 }
